af50c08b485c518840210ca17d802d50fa06fd46
razor267 <razor267@mail.ru>
2021-11-05 18:17:30 +0300

    add post and message les 31

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -20,6 +20,13 @@ const Dialogs = (props) => {
     // let messagesElements = props.state.messages.map(m => <Message message={m.message}/>);
     let messagesElements = props.state.dialogs[GetUserUrl()].dialog.map(m => <Message message={m.message} iOrNot={m.iOrNot} avatar={props.state.dialogs[GetUserUrl()].avatar}/>);
 
+    let newMessageElement = React.createRef();
+
+    let newMessage = () => {
+        let text = newMessageElement.current.value;
+        alert(text);
+    }
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
@@ -27,6 +34,14 @@ const Dialogs = (props) => {
             </div>
             <div className={s.messages}>
                 {messagesElements}
+            </div>
+            <div className={s.newMessage}>
+                <div>
+                    <textarea ref={newMessageElement}></textarea>
+                </div>
+                <div>
+                    <button onClick={newMessage}>Add post</button>
+                </div>
             </div>
         </div>
     )
