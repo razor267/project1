89228f37b300e31a16141af5d675cf807bfe7494
razor267 <razor267@mail.ru>
2021-11-20 17:31:57 +0300

    add class component. LES 53

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,45 +1,21 @@
 import React from "react";
 import styles from './Users.module.css';
-import axios from "axios";
 import userPhoto from "../../assets/images/avatar.jpg";
+import axios from "axios";
 
-let Users = (props) => {
+class Users extends React.Component {
 
-    if (props.users.length === 0) {
-        let tar = axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items);
+    constructor(props) {
+        super(props);
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+            this.props.setUsers(response.data.items);
         })
-        /*       props.setUsers([
-                   {
-                       id: 1,
-                       photoUrl: 'https://proslang.ru/wp-content/uploads/2019/03/avatarka_1-300x300.jpg',
-                       followed: false,
-                       fullName: 'Petr',
-                       status: 'I am a boss',
-                       location: {city: 'Moscow', country: 'Russia'}
-                   },
-                   {
-                       id: 2,
-                       photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHRNmLw6jPNkjheKybsLrQ_MG5yB5Foe4Mj7WYKFUay-7bsHpRRNi2TE35jznFo3TNjPQ&usqp=CAU',
-                       followed: true,
-                       fullName: 'Maksim',
-                       status: 'I am a boss too',
-                       location: {city: 'Moscow', country: 'Russia'}
-                   },
-                   {
-                       id: 3,
-                       photoUrl: 'https://content.freelancehunt.com/textattach/00251/5fcec/635497/1581419181452.png',
-                       followed: false,
-                       fullName: 'Dennis',
-                       status: 'I am a boss too',
-                       location: {city: 'Kiev', country: 'Ukraine'}
-                   }
-               ]);*/
     }
 
-    return <div>
-        {
-            props.users.map(u => <div key={u.id}>
+    render() {
+        return <div>
+            {
+                this.props.users.map(u => <div key={u.id}>
             <span>
                 <div>
                     <img
@@ -49,14 +25,14 @@ let Users = (props) => {
                 <div>
                     {u.followed
                         ? <button onClick={() => {
-                            props.unfollow(u.id)
+                            this.props.unfollow(u.id)
                         }}>Unfollow</button>
                         : <button onClick={() => {
-                            props.follow(u.id)
+                            this.props.follow(u.id)
                         }}>Follow</button>}
                 </div>
             </span>
-                <span>
+                    <span>
                     <span>
                         <div>{u.name}</div>
                         <div>{u.status}</div>
@@ -66,9 +42,10 @@ let Users = (props) => {
                         <div>{"u.location.city"}</div>
                     </span>
                 </span>
-            </div>)
-        }
-    </div>
+                </div>)
+            }
+        </div>
+    }
 }
 
 export default Users;
