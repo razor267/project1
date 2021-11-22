55a6932bd566bb3fd1b15de45645b813dcde8df2
razor267 <razor267@mail.ru>
2021-11-22 22:26:20 +0300

    cookie, login, auth. LES 61

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import './App.css';
-import Header from './components/Header/Header';
 import NavBar from './components/Nav/NavBar';
 import {Route} from "react-router-dom";
 import News from "./components/News/News";
@@ -9,11 +8,12 @@ import Settings from "./components/Settings/Settings";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderContainer from "./components/Header/HeaderContainer";
 
 const App = () => {
     return (
         <div className='app-wrapper'>
-            <Header/>
+            <HeaderContainer/>
             <NavBar/>
             <div className='app-wrapper-content'>
                 <Route path='/dialogs' render={() => <DialogsContainer/>}/>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import s from './Header.module.css';
+import {NavLink} from "react-router-dom";
 
-const Header = () => {
+const Header = (props) => {
     return (
         <header className={s.header}>
-            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrCGuyFs-eh7eEkapIpuvWtQ9Gpwu04S5amQ&usqp=CAU'/>
+            <img
+                src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrCGuyFs-eh7eEkapIpuvWtQ9Gpwu04S5amQ&usqp=CAU'/>
+
+            <div className={s.loginBlock}>
+                {props.isAuth ? props.login : <NavLink to={'login'}>Login</NavLink>}
+            </div>
         </header>
     );
 }
