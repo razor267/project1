bbe3288f73c66b363056989b2ae2240f69156ced
razor267 <razor267@mail.ru>
2022-03-23 23:57:19 +0300

    useHistory, filter, search, query string. LES 2/16

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -12,10 +12,12 @@ import {
     getUsers,
     getUsersFilter
 } from '../../redux/usersSelectors'
+import {useHistory} from 'react-router-dom'
+import * as queryString from 'querystring'
 
-type PropsType = {
-}
+type PropsType = {}
 
+type QueryParamsType = { term?: string; page?: string; friend?: string }
 export const Users: FC<PropsType> = (props) => {
 
     const users = useSelector(getUsers)
@@ -26,10 +28,31 @@ export const Users: FC<PropsType> = (props) => {
     const followUnfollowInProgress = useSelector(getFollowUnfollowInProgress)
 
     const dispatch = useDispatch()
+    const history = useHistory()
 
     useEffect(() => {
-        dispatch(requestUsers(currentPage, pageSize, filter))
-    },[])
+        const parsed = queryString.parse(history.location.search.substring(1)) as QueryParamsType
+        let actualPage = currentPage
+        let actualFilter = filter
+        if (!!parsed.page) actualPage = Number(parsed.page)
+        if (!!parsed.term) actualFilter = {...actualFilter, term: parsed.term as string}
+        if (!!parsed.friend) actualFilter = {
+            ...actualFilter,
+            friend: parsed.friend === 'null' ? null : parsed.friend === 'true' ? true : false
+        }
+        dispatch(requestUsers(actualPage, pageSize, actualFilter))
+    }, [])
+
+    useEffect(() => {
+        const query:QueryParamsType = {}
+        if (!!filter.term) query.term = filter.term
+        if (filter.friend !== null) query.friend = String(filter.friend)
+        if (currentPage !== 1) query.page = String(currentPage)
+        history.push({
+            pathname: '/users',
+            search: queryString.stringify(query)
+        })
+    }, [filter, currentPage])
 
     const onPageChanged = (pageNumber: number) => {
         dispatch(requestUsers(pageNumber, pageSize, filter))
--- src/components/Users/UsersSearchForm.tsx
+++ src/components/Users/UsersSearchForm.tsx
@@ -1,16 +1,17 @@
 import {Field, Form, Formik} from 'formik'
 import React from 'react'
 import {FilterType} from '../../redux/usersReducer'
+import {useSelector} from 'react-redux'
+import {getUsersFilter} from '../../redux/usersSelectors'
 
 const usersSearchFormValidate = (values: any) => {
     const errors = {}
     return errors
 }
-
+type FriendFormType = 'true' | 'false' | 'null'
 type FormType = {
     term: string,
-    friend: string
-    // friend: 'true' | 'false' | 'null'
+    friend: FriendFormType
 }
 
 type PropsType = {
@@ -18,6 +19,8 @@ type PropsType = {
 }
 
 export const UsersSearchForm: React.FC<PropsType> = React.memo((props) => {
+
+    const filter = useSelector(getUsersFilter)
 
     const submit = (values: FormType, {setSubmitting}: { setSubmitting: (isSubmitting: boolean) => void }) => {
         const filter: FilterType = {
@@ -31,7 +34,8 @@ export const UsersSearchForm: React.FC<PropsType> = React.memo((props) => {
 
     return <div>
         <Formik
-            initialValues={{term: '', friend: 'null'}}
+            enableReinitialize
+            initialValues={{term: filter.term, friend: String(filter.friend) as FriendFormType}}
             validate={usersSearchFormValidate}
             onSubmit={submit}
         >
